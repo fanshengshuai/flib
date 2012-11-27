99b6a2ab69e52c44b885737a30aaadcd444c4b68
root <root@143.(none)>
2012-11-27 09:01:53 +0800

    增加站点采集功能

--- library/Uploader.php
+++ library/Uploader.php
@@ -11,19 +11,55 @@
  */
 class Uploader {
 
-    public function saveAttach($field, $obj) {
+    public function setFileType($type) {
+    }
+
+    public function getFileType($file_name) {
+        $ext_arr = array(
+            'pic' => array('gif', 'jpg', 'jpeg', 'png', 'bmp'),
+            'flash' => array('swf', 'flv'),
+            'media' => array('swf', 'flv', 'mp3', 'wav', 'wma', 'wmv', 'mid', 'avi', 'mpg', 'asf', 'rm', 'rmvb'),
+            'file' => array('doc', 'docx', 'xls', 'xlsx', 'ppt', 'htm', 'html', 'txt', 'zip', 'rar', 'gz', 'bz2'),
+        );
+
+        $temp_arr = explode(".", $file_name);
+        $file_ext = array_pop($temp_arr);
+        $file_ext = trim($file_ext);
+        $file_ext = strtolower($file_ext);
+
+        if (in_array($file_ext, $ext_arr['pic'])) {
+            return 'pic';
+        }
+    }
+
+    public static function saveFile($field) {
+        $uploader = new Uploader;
+        $photo_file = "image/" . date('Ymd') . '/' . date('YmdHis') . ".attach";
+        $upload_file = $uploader->saveAttach('pic_url', $photo_file);
+        return $upload_file['file_path'];
+    }
+
+    public function saveAttach($field, $obj=null) {
+
+        if (!$obj) {
+        }
+
 
         // 处理扩展名
         if (strpos($obj, '.attach')) {
             $file_ext = addslashes(strtolower(substr(strrchr($_FILES[$field]['type'], '/'), 1, 10)));
+
+            if ($file_ext == 'jpeg') $file_ext = 'jpg';
             //echo $file_ext;exit;
             $obj = str_replace('.attach', '.' . $file_ext, $obj);
         }
 
+        //echo $obj;
+
         $attach_url = $obj;
 
         if (strpos($obj, APP_ROOT) === false) {
-            $obj = APP_ROOT . "www/attachs/" . $obj;
+            $obj = APP_ROOT . "public/attachs/" . $obj;
         }
 
         $attach_dir = dirname($obj);
@@ -45,7 +81,9 @@ class Uploader {
                 'file_path' => $attach_url,
             );
 
-            return $attachDAO->add($data);
+            $attachDAO->add($data);
+
+            return $data;
         }
 
         return false;
